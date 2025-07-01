ce2266b861052532558b9dd3d354318d57326035
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-07-01 15:02:27 +0530

    Update User.ts

--- models/User.ts
+++ models/User.ts
@@ -1,41 +1,20 @@
 import mongoose from "mongoose";
 
-const AdminSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
-    _id: {
-      type: Number,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    role: {
-      type: String,
-      enum: ["user", "admin", "owner"],
-      default: "user",
-    },
-    date: {
-      type: String,
-      required: true,
-    },
-    time: {
-      type: String,
-      required: true,
-    },
+    _id: { type: Number, required: true },
+    name: { type: String, required: true },
+    dc_id: { type: Number, default: null },
+    date: { type: String, required: true },
+    time: { type: String, required: true },
+    permission: { type: Boolean, default: true },
   },
   {
     timestamps: false,
-    collection: "admins",
+    collection: "users",
     _id: false,
   }
 );
 
-export default mongoose.models.Admin ||
-  mongoose.model("Admin", AdminSchema, "admins");
+export default mongoose.models.User ||
+  mongoose.model("User", UserSchema, "users");
