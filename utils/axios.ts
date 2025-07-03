6d4f00014cf779b2f891d7d9a16fa0f215080723
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-07-03 10:42:54 +0530

    Create axios.ts

---
+++ utils/axios.ts
@@ -0,0 +1,38 @@
+import Cookies from "js-cookie";
+import axios, { AxiosError, AxiosInstance } from "axios";
+
+const withAuth = (): AxiosInstance => {
+  const instance = axios.create({
+    baseURL: "",
+    timeout: 15000,
+  });
+
+  instance.interceptors.request.use((config) => {
+    const token = Cookies.get("cypher-session");
+
+    console.log("token", token);
+
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+
+  instance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      console.error("[API ERROR]", error);
+      throw new AxiosError(
+        error.response?.data?.message || "API request failed",
+        error.code,
+        error.config,
+        error.request,
+        error.response
+      );
+    }
+  );
+
+  return instance;
+};
+
+export const apiClient = withAuth();
