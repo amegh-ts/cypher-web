709545cd1ca8436d2230b38b51e91a0a60c96cbc
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-07-23 18:01:22 +0530

    Create formatFileSize.ts

---
+++ utils/formatFileSize.ts
@@ -0,0 +1,8 @@
+export function formatFileSize(bytes: number) {
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  if (bytes === 0) return "0 Byte";
+  const i = Number.parseInt(
+    Math.floor(Math.log(bytes) / Math.log(1024)).toString()
+  );
+  return Math.round((bytes / Math.pow(1024, i)) * 100) / 100 + " " + sizes[i];
+}
