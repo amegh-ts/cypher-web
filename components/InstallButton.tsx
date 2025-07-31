d74bc772ceb438e7996bfb7a35490aacd4944ebc
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-07-31 12:32:21 +0530

    Create InstallButton.tsx

---
+++ components/InstallButton.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
+const InstallButton = () => {
+  const [deferredPrompt, setDeferredPrompt] = useState<Event | null>(null);
+  const [isInstallable, setIsInstallable] = useState(false);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e: Event) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+      setIsInstallable(true);
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () =>
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+  }, []);
+
+  const handleInstallClick = async () => {
+    if (deferredPrompt) {
+      (deferredPrompt as any).prompt();
+      const { outcome } = await (deferredPrompt as any).userChoice;
+      setDeferredPrompt(null);
+      setIsInstallable(false);
+      console.log(`User response to the install prompt: ${outcome}`);
+    }
+  };
+
+  return (
+    <>
+      {isInstallable && (
+        <Button onClick={handleInstallClick} className="install-button">
+          Install App
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default InstallButton;
