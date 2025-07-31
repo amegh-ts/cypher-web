1a48b4b799a3ec3f20fd2b97f2aa36618aaca2db
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-07-31 18:14:24 +0530

    Update AdvanceSidebar.tsx

--- components/AdvanceSidebar.tsx
+++ components/AdvanceSidebar.tsx
@@ -19,7 +19,7 @@ export default function AdvanceSidebar({
       <Sidebar />
       <main
         className={cn(
-          "min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
+          "min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300 pb-[100px] md:pb-0",
           !settings.disabled && (!getOpenState() ? "lg:ml-[90px]" : "lg:ml-72")
         )}
       >
